09b85836ff906b7f09175839d9b65a081d507734
synod2 <synod2@naver.com>
2020-08-12 06:28:05 +0000

    0812_master

--- git.sh
+++ git.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 
-msg="$(date '+%m')$(date '+%d')_notebook"
+msg="$(date '+%m')$(date '+%d')_master"
 
 git add .
 git commit -m $msg 
-git push -u origin notebook
+git push -u origin master
